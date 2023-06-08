cabdb50116fd3bbefff0dd9e8572e9779c1b3c6a
CircleCI <matiastucci@gmail.com>
2023-06-08 16:19:25 +0200

    fix path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { NativeModules } from 'react-native';
-import { PDFViewCtrl } from './src/PDFViewCtrl/PDFViewCtrl';
-import { DocumentView } from './src/DocumentView/DocumentView';
-import { Config } from './src/Config/Config';
-import * as AnnotOptions from './src/AnnotOptions/AnnotOptions';
+import { PDFViewCtrl } from './PDFViewCtrl/PDFViewCtrl';
+import { DocumentView } from './DocumentView/DocumentView';
+import { Config } from './Config/Config';
+import * as AnnotOptions from './AnnotOptions/AnnotOptions';
 const RNPdftron = NativeModules.RNPdftron;
 export { RNPdftron, PDFViewCtrl, DocumentView, Config, AnnotOptions, };
